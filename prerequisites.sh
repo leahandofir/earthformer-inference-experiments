05f399cefc11333a38ad2096731a63aef09d3146
leahandofir <132069316+leahandofir@users.noreply.github.com>
2023-05-14 14:35:15 +0300

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,6 +1,18 @@
+# cuda dependencies
+# install CUDA 11.6.2 https://stackoverflow.com/questions/50560395/how-to-install-cuda-in-google-colab-gpus
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.0-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+apt-get update
+apt-get -y install cuda-11-6
+# symlink to be the current CUDA
+!rm /usr/local/cuda
+!ln -s /usr/local/cuda-11.6 /usr/local/cuda
 # earthformer dependencies
 pip install --upgrade pip
-python3 -m pip install torch==2.0.0+cu118 torchvision -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 torchtext==0.13.1 torchdata==0.4.1 --extra-index-url https://download.pytorch.org/whl/cu116
 python3 -m pip install "pytorch_lightning>=1.6.4,<1.8.0"
 python3 -m pip install xarray netcdf4 opencv-python
 git clone https://github.com/amazon-science/earth-forecasting-transformer.git
