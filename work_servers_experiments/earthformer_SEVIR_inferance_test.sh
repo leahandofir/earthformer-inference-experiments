e3a4561227c77d66177b01feca70f6befd73f3e1
leahandofir <leahandofir@gmail.com>
2023-05-16 21:27:22 +0300

    test

--- work_servers_experiments/earthformer_SEVIR_inferance_test.sh
+++ work_servers_experiments/earthformer_SEVIR_inferance_test.sh
@@ -4,5 +4,5 @@ aws s3 cp --no-sign-request --recursive --region us-west-2 s3://sevir/data/vil/2
 aws s3 cp --no-sign-request --region us-west-2 s3://sevir/CATALOG.csv ~/leah/earthformer/datasets/sevir/CATALOG.csv
 # edit config
 python ~/leah/earthformer/work_servers_experiments/edit_SEVIR_config.py
-# run train
+# run train on the server
 MASTER_ADDR=localhost MASTER_PORT=10001 python train_cuboid_sevir.py --gpus 1 --cfg my_cfg_sevir.yaml --ckpt_name last.ckpt --save tmp_sevir
