c1f26dea8a95b137d109be02f85625840551fd20
leahandofir <132069316+leahandofir@users.noreply.github.com>
2023-06-21 10:53:51 +0300

    Update prerequisites.sh

--- work_servers_experiments/prerequisites.sh
+++ work_servers_experiments/prerequisites.sh
@@ -8,7 +8,7 @@ python3 -m pip install "pytorch_lightning>=1.6.4,<1.8.0"
 python3 -m pip install xarray netcdf4 opencv-python
 git clone https://github.com/amazon-science/earth-forecasting-transformer.git
 cd earth-forecasting-transformer
-python3 -m pip install -U -e . --no-build-isolation
+python3 -m pip install -U -e . --no-build-isolation --extra-index-url --trusted-host # extra flags are important for pip freeze https://stackoverflow.com/questions/35656557/pip-freeze-breaks-with-package-installation
 pip install -v --disable-pip-version-check --no-cache-dir pytorch-extension git+https://github.com/NVIDIA/apex.git
 # install aws cli to check out sevir dataset
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
